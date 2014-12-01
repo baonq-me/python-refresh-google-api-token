34162b94f0f9171711ef3f1d1322a2a4768cb46e
Ice5050 <ice50505050@hotmail.com>
2014-12-01 10:16:09 +0700

    init project

---
+++ refresh_token.py
@@ -0,0 +1,14 @@
+import httplib2
+import urllib
+
+h = httplib2.Http(".cache")
+url = 'https://accounts.google.com/o/oauth2/token'
+body = { 'client_secret': 'xxx',
+  'grant_type': 'refresh_token',
+  'refresh_token': 'xxx',
+  'client_id': 'xxx'
+  }
+headers = {'Content-type': 'application/x-www-form-urlencoded', 'cache-control': 'no-cache'}
+response, content = h.request(url, 'POST', headers=headers, body=urllib.urlencode(body))
+
+print content
