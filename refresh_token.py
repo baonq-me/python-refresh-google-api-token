549d94b290985df3a0b16df6e29ed5cd07a02e91
quocbao <quocbao747@gmail.com>
2018-06-14 08:49:58 +0700

    Update refresh_token.py

--- refresh_token.py
+++ refresh_token.py
@@ -1,14 +1,19 @@
+#!/usr/bin/python
+
 import httplib2
 import urllib
 
 h = httplib2.Http(".cache")
-url = 'https://accounts.google.com/o/oauth2/token'
-body = { 'client_secret': 'xxx',
+#url = 'https://accounts.google.com/o/oauth2/token'
+url = 'https://www.googleapis.com/oauth2/v4/token'
+body = 
+{ 
+  'client_secret': 'xxx',
   'grant_type': 'refresh_token',
   'refresh_token': 'xxx',
   'client_id': 'xxx'
-  }
+}
 headers = {'Content-type': 'application/x-www-form-urlencoded', 'cache-control': 'no-cache'}
 response, content = h.request(url, 'POST', headers=headers, body=urllib.urlencode(body))
 
-print content
+print(content)
